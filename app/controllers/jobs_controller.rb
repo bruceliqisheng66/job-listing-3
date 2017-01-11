f03bcd3521a921dfad6060c3813121f35b41469c
li qi sheng <bruce.liqisheng66@gmail.com>
2017-01-11 12:17:16 +0800

    implement part 2

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -43,6 +43,13 @@ class JobsController < ApplicationController
     end
   end
 
+  def require_is_admin
+    unless current_user.admin?
+      flash[:alert] = 'You are not admin'
+      redirect_to root_path
+    end
+  end
+
   private
 
   def job_params
